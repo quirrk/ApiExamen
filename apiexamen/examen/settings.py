80f1dfae3c73fd1965a0af0c68aa1f054d8aaa79
Nicolas Reyes <nicolas.r.suarez@gmail.com>
2018-12-12 17:05:38 -0300

    se añade la whitelist para pruebas

--- apiexamen/examen/settings.py
+++ apiexamen/examen/settings.py
@@ -126,3 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:8887/'
+)
+CORS_ORIGIN_REGEX_WHITELIST = (
+    'http://127.0.0.1:8887/'
+)
